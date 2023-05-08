503868c7292f861ad9f7f4fc4cd3dfb7f6bc36ce
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-05-08 18:37:38 +0100

    Task 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3, word_last_2, middle_word = word[0:3], word[-2:], word[1:8]
+word_first_3, word_last_2, middle_word = word[0:3], word[-2:], word[1:-1]
 print(f"First 3 letters: {word_first_3:s}")
 print(f"Last 2 letters: {word_last_2:s}")
 print(f"Middle word: {middle_word:s}")
