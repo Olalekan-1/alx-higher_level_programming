7a6561a7f5f0c322bb068a554601d9f2471059bf
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-05-15 15:35:33 +0100

    Task 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -16,16 +16,49 @@ void print_python_list_info(PyObject *p)
 	Py_ssize_t size, i;
 	PyObject *itemstr;
 	const char *itemCstr;
+	size_t itemAllocated;
 
+	if (!PyList_Check(p))
+	{
+		fprintf(stderr, "Invalid list object\n");
+		return;
+	}
 	size = PyList_Size(p);
+
+	if (size < 0)
+	{
+		fprintf(stderr, "Failed to get list size\n");
+		return;
+	}
+	printf("[*] Size of the Python List = %zd\n", size);
 
 	for (i = 0; i < size; i++)
 	{
 		item = PyList_GetItem(p, i);
-		itemstr = PyObject_Str(item);
+		itemstr =  PyObject_Str(item);
 		itemCstr = PyUnicode_AsUTF8(itemstr);
-		printf("%s\n", itemCstr);
+
+		if (itemstr == NULL || itemCstr == NULL)
+		{
+			Py_DECREF(itemstr);
+			Py_DECREF(item);
+			fprintf(stderr, "Failed to convert item to string at index %zd\n", i);
+			return;
+		}
+		itemAllocated = (item->ob_type->tp_basicsize);
+		printf("[*] Allocated = %zd\n", itemAllocated);
+		printf("Element %zd: %s\n", i, itemCstr);
+		
 		Py_DECREF(itemstr);
+	/*	Py_DECREF(item);
+		PyObject_Free((void *)itemAllocated);*/
 	}
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		Py_DECREF(item);
+	}
+	/*PyObject_Free((void *)itemAllocated);*/
+
 }
 
