3f0721063796d4e1da67dd244744fbc0cb6bcd36
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-05-16 22:25:17 +0100

    TAsk 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 
-def add(x, y):
-    return x + y
-
-
-def sub(x, y):
-    return x - y
-
+from magic_calculation_102 import add, sub
 
 def magic_calculation(a, b):
-    from magic_calculation import add, sub
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
         return c
     else:
-        return sub(a, b)
+        return ( sub(a, b))
+    return  0
---
+++ 0x02-python-import_modules/magic_calculation_102.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
