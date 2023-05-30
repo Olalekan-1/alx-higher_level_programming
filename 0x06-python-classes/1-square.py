8d29293d34d11578d1828bed08ee5c8392ba1a8e
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-05-31 00:01:05 +0100

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-""" A square class with a private instance attribute """
+""" A square class with a private instance attribute"""
+
 
 class Square:
-    """ A square class with a private instance attribute """
+    """ Square with size attribute"""
     def __init__(self, size):
-        """
-        parameter
-        _________
-        size : int
-            The size of the square
+        """ An instance of Square created with size atrribute
+        Args:
+            size (int): size of the square
         """
         self._size = size
-
-    def area(self):
-        """ Define and return the area of the square """
-        return self._size ** 2
-
-    def perimeter(self):
-        """ Define and return the perimeter of the square """
-        return 4 * self._size
