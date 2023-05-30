d0c0aca11afffc8a30b0b70a07dba7b550b136a8
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-05-30 22:28:38 +0100

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """ A square class with a private instance attribute """
+    def __init__(self, size):
+        """
+        parameter
+        _________
+        size : int
+            The size of the square
+        """
+        self._size = size
