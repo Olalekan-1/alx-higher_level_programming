faa40ada000587688f70b947e24fb3fc82c4f6d4
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-05-31 01:06:15 +0100

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+""" A square class with a private instance attribute"""
+
+
+class Square:
+    """ Square with size attribute"""
+    def __init__(self, size=0):
+        """ An instance of Square created with size atrribute
+        Args:
+            size (int): size of the square
+        Raises:
+            ValueError: If the size is less than 0.
+            TypeError: If the size is not an integer.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Define and return the area of the square
+        Returns:
+            The area of the Square
+        """
+        return self.__size ** 2
