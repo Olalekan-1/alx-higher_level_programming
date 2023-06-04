9975c14f9598b5fd3751d2200439e86e11544ecb
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-04 22:50:50 +0100

    Task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,29 +1,38 @@
 #!/usr/bin/python3
 """ This module contains a function text_indentation(). for example
 >>> text_indentation("i am Olalekan: Olalekan, do you really know me? me.")
-i am olalekan:
+i am Olalekan:
 <BLANKLINE>
 Olalekan, do you really know me?
 <BLANKLINE>
-me.
-"""
+me."""
+
 
 def text_indentation(text):
-    """ This function indent text according ':?.' delimiter
+    """ This function indent text according ',:?.' delimiter
     Args:
         text (string): The input text
     Raises:
         TypeError: if text is not a string
         """
-    j = 0
     if type(text) is not str:
         raise TypeError("text must be a string")
-    while j < len(text):
-        if text[j] in ':?.':
-            print(text[j], end='')
-            print('\n')
-            if text[j + 1] == ' ':
-                j += 1
+
+    delimiter_chars = ['.', '?', ':']
+    result = ""
+    i = 0
+
+    while i < len(text):
+        if text[i] in delimiter_chars:
+            result += text[i]
+            if i + 1 < len(text) and text[i + 1] == ' ':
+                i += 1
+            result += '\n\n'
         else:
-            print(text[j], end='')
-        j += 1
+            result += text[i]
+        i += 1
+
+    if result.endswith('\n\n'):
+        result = result[:-2]
+
+    print(result, end='')
