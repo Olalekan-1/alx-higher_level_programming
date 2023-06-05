606e77a6f9e83a54e8f580bfdcf88a5223804987
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-05 14:48:16 +0100

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("heigth must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     @property
