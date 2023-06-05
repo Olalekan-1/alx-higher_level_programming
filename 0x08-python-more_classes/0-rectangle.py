9ffa7dc55f0b1566fe52e3ce02842cb21e75f7b7
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-05 13:18:24 +0100

    Task 1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" This module contains an empty rectangle class
+"""
+
+
+class Rectangle:
+    """ An empty rectangle class
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+ This repo contains more solutions to more tasks on python Classes and object
