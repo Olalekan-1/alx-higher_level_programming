d20e63afba2b9f95886bcf7251992282aca57bd7
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-10 15:26:16 +0100

    task7

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -7,9 +7,6 @@ This module contain a Class definition
 
 class BaseGeometry:
     """ A Base geometry class """
-    def __int__(self):
-        """ an instance created """
-        pass
 
     def area(self):
         """ an area method
---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+This module contain a Class definition
+"""
+
+
+class BaseGeometry:
+    """ A Base geometry class """
+
+    def area(self):
+        """ an area method
+        Raises:
+            Exception:...
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ A function that assign name to a value
+        and validate the value if it is an integer
+        Raises:
+            TypeError: if value is not int
+            ValueError: if int <= 0
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        name = value
+
