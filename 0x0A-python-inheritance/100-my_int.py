c15bb80e2b6997197a1c6269598c7c2a68f2ea99
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-12 17:38:30 +0100

    Task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" this module contains MyInt class
+"""
+
+
+class MyInt(int):
+    """ MyInt class """
+    def __eq__(self, other):
+        """ '==' inverted"""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """ '!=" inverted"""
+        return super().__eq__(other)
