fd9f2fa7f1ccaf4425d2f103f8bb6d201516a968
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-13 23:55:12 +0100

    Append after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" This module contains a append_after()"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    ''' Append a new_string if search_stringis found in a line
+    '''
+    with open(filename, 'r+', encoding='utf-8') as file:
+        line = file.readlines()
+        file.seek(0)
+        for lin in line:
+            file.write(l)
+            if search_string in lin:
+                file.write(new_string)
