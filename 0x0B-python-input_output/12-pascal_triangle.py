8b8162fd4c201631aac032595c23086825e05dab
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-13 17:34:04 +0100

    Pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" This module contains a pascal_tringle().
+"""
+
+
+def pascal_triangle(n):
+    """ prints pascal triangle
+    with list of lists"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
