60c6ab9d465a0b6fe3754ef8728832458d16cefe
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-15 14:25:36 +0100

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" This module contains aBase class
+"""
+
+
+class Base:
+    """
+    A base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ An instance of base class created
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
