ce462fb388ccc8d1b6dc4b36c4501d3e638fa6fc
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-17 20:57:37 +0100

    update base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 
 """ This module contains aBase class
 """
+import json
 
 
 class Base:
@@ -18,3 +19,31 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ To represent in json format"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return list()
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Return the object of json_string"""
+        if not json_string or json_string == '':
+            return list()
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ save json strings to json file"""
+        new = []
+        filename = cls.__name__ + ".json"
+        for item in list_objs:
+            rep = item.to_dictionary()
+            new.append(rep)
+
+        json_data = cls.to_json_string(new)
+
+        with open(filename, 'w') as file:
+            file.write(json_data)
