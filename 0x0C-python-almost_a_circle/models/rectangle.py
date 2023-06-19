ee73d61a6dd666ff6be1a45cd0c9d948dadb3daa
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-19 19:45:01 +0100

    update rect

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -159,7 +159,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must be >0")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def to_dictionary(self):
