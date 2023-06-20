d5f21aa5c901d8f99c18db1014018f6e9bbec52b
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-20 23:25:02 +0100

    Test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+"""
+"""
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 import turtle
-"""
-"""
+from unittest.mock import patch
 
 
 class Testbase(unittest.TestCase):
@@ -22,16 +23,20 @@ class Testbase(unittest.TestCase):
 
     def test_instance_id(self):
         """ Test the instance id"""
-        self.assertEqual(self.poly1.id, 1)
-        self.assertEqual(self.poly2.id, 2)
+        self.assertEqual(self.poly1.id, 4)
+        self.assertEqual(self.poly2.id, 5)
         self.assertEqual(self.poly3.id, 8)
-        self.assertEqual(self.poly4.id, 3)
+        self.assertEqual(self.poly4.id, 6)
 
     def test_to_json_string(self):
         """ Test the to_json_string()."""
-        self.assertEqual(self.poly1.to_json_string(None), [])
-        self.assertEqual(self.poly2.to_json_string([]), [])
+        self.assertEqual(self.poly1.to_json_string(None), '[]')
+        self.assertEqual(self.poly2.to_json_string([]), '[]')
         self.assertEqual(self.poly3.to_json_string([{'id': 12}]), '[{"id": 12}]')
+
+    def test_from_json_string(self):
+        """ Test the from_json_string():"""
+        self.assertEqual(self.poly1.from_json_string(None), [])
 
 
 if __name__ == '__main__':
