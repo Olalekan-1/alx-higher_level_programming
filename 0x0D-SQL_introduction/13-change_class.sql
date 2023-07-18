5938146e1ea2266abfab34042eb88917f32f7b2f
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-07-18 16:03:14 +0100

    add solutions

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- delete record where score is less than or equal 5
+
+DELETE FROM second_table WHERE score<=5;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- computes the average of score column
+
+SELECT AVG(score) AS average FROM second_table;
