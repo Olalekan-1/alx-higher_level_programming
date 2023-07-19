4dc1959e3f17eb1400086f468b79e41f33bfad6e
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-07-19 13:49:59 +0100

    add solutions

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- show the priviledges that user has 
 
 SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR 'user_0d_2''@''localhost';
+SHOW GRANTS FOR user_0d_2@localhost;
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
 -- create a new my mysql server user
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
