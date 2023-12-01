18fa5a54eaf48e644be2a5187fbf1e408505c1a4
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-12-01 22:33:12 +0100

    add solutions

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+fetch an url using the request package
+"""
+
+import requests
+
+url = "https://alx-intranet.hbtn.io/status"
+
+response = requests.get(url)
+
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
