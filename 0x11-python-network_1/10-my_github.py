c37b4f35cf4061e63cc0a2075b3bef13498de9c0
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-12-02 05:45:46 +0100

    add solutions

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,26 +7,10 @@ UserId at Github.
 
 import requests
 import sys
-import base64
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
-
-    username = sys.argv[1]
-    token = sys.argv[2]
-
-    credentials = f"{username}:{token}"
-    base64_credentials = base64.b64encode(credentials.encode
-                                          ('utf-8')).decode('utf-8')
-    headers = {'Authorization': f'Basic {base64_credentials}'}
-
-    # Make the request to the GitHub API to get user information
-    url = 'https://api.github.com/user'
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        # If the request is successful, display the user id
-        user_id = response.json().get('id')
-        print(user_id)
-    else:
-        # If there is an error, display the status code and response text
-        print("None")
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
