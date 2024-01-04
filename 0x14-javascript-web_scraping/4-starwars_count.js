88422c6adbcb4623dc354f27f68b6db6aea44af3
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-01-04 12:52:40 +0100

    add solution

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+function displayCharacterOccurrence () {
+  let count = 0;
+  const url = process.argv[2];
+
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const content = JSON.parse(body);
+      const elem = content.results;
+      for (let i = 0; i < elem.length; i++) {
+        if (elem[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) { count += 1; }
+      }
+
+      console.log(count);
+    }
+  });
+}
+
+displayCharacterOccurrence();
